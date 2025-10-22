4772dc1118c545aa4a35418a6d58535572804b19
Colin <cpireyre@student.42.fr>
2025-10-22 16:09:25 +0300

    Track game state & send mock XHR when winning

--- game.js
+++ game.js
@@ -3,6 +3,11 @@
 const PLAYING_FIELD_WIDTH  = 20;
 const PLAYING_FIELD_HEIGHT = 15;
 
+const States = Object.freeze({
+  START:      Symbol('start'),
+  PLAYING:    Symbol('playing'),
+  GAME_OVER:  Symbol('game_over'),
+});
 G = {
   height: PLAYING_FIELD_WIDTH, width: PLAYING_FIELD_WIDTH,
   ball: {
@@ -14,16 +19,19 @@ G = {
   },
   p1: {
     height: 2.6, width: 0.2,
+    name: "Player 1",
     score: 0,
     speed: 20,
     x: 8.5, z: 0,
   },
   p2: {
     height: 2.6, width: 0.2,
+    name: "Player 2",
     score: 0,
     speed: 20,
     x: -8.5, z: 0,
   },
+  state: States.START,
 };
 
 function move(v, u, speed, dt) {
@@ -34,13 +42,7 @@ function move(v, u, speed, dt) {
   return (v);
 }
 
-b = BABYLON; v3 = BABYLON.Vector3;
 const canvas = document.getElementById("canvas");
-
-Engine = new b.Engine(canvas);
-S = new b.Scene(Engine);
-S.clearColor = new BABYLON.Color4(0, 0, 0, 0);
-
 function intersect(ball, p) {
   const W = p.width;
   const H = p.height / 2;
@@ -57,32 +59,6 @@ function collide(ball, p) {
   }
 }
 
-S.camera = new b.FreeCamera("camera", new v3(0, 15, 8), S);
-S.ground = b.MeshBuilder.CreateGround("ground",
-  {width: G.width, height: PLAYING_FIELD_HEIGHT}, S);
-S.sphere = b.MeshBuilder.CreateSphere("sphere", {diameter: G.ball.diameter}, S);
-
-S.camera.setTarget(v3.Zero());
-S.ground.position = new v3(0,-1,0);
-
-S.light3 = new b.PointLight("light3", new v3(0, 2, 0), S);
-S.light3.intensity = 0.1;
-
-Object.assign(S, {
-  lpaddle: b.MeshBuilder.CreateCapsule("lpaddle", {
-    height: G.p1.height, radius: G.p1.width, orientation: new v3(0, 0, 1)
-  }, S),
-  rpaddle: b.MeshBuilder.CreateCapsule("rpaddle", {
-    height: G.p2.height, radius: G.p2.width, orientation: new v3(0, 0, 1)
-  }, S)
-});
-
-const paddleColor = new b.Color3(1,1,1);
-[S.lpaddle, S.rpaddle, S.sphere].forEach(mesh => {
-  mesh.material = new b.StandardMaterial(`${mesh.name}_mat`, S);
-  mesh.material.emissiveColor = paddleColor;
-});
-
 keys_down = new Set();
 document.addEventListener('keydown', (e) => keys_down.add(e.code));
 document.addEventListener('keyup', (e) => keys_down.delete(e.code));
@@ -92,25 +68,49 @@ last_time_ms = 0;
 function update(current_time_ms) {
   const delta_ms = (current_time_ms - last_time_ms) / 1000;
   last_time_ms = current_time_ms;
-  if (keys_down.has('KeyW') && G.p1.z > -5)
-    move(G.p1, {z: -1}, G.p1.speed, delta_ms);
-  if (keys_down.has('KeyS') && G.p1.z < 5)
-    move(G.p1, {z: 1}, G.p1.speed, delta_ms);
-  if (keys_down.has('KeyI') && G.p2.z > -5)
-    move(G.p2, {z: -1}, G.p2.speed, delta_ms);
-  if (keys_down.has('KeyK') && G.p2.z < 5)
-    move(G.p2, {z: 1}, G.p2.speed, delta_ms);
-  if (G.ball.dir.x > 0) collide(G.ball, G.p1);
-  if (G.ball.dir.x < 0) collide(G.ball, G.p2);
-  move(G.ball, G.ball.dir, G.ball.speed, delta_ms);
-  if (Math.abs(G.ball.z) > 6.3) {
-    if (G.ball.dir.z < 0 && G.ball.z < 0) G.ball.dir.z *= -1;
-    if (G.ball.dir.z > 0 && G.ball.z > 0) G.ball.dir.z *= -1;
-  }
-  if (Math.abs(G.ball.x) > G.width / 2) {
-    if (G.ball.x < 0) G.p1.score += 1;
-    if (G.ball.x > 0) G.p2.score += 1;
-    G.ball.z = 0; G.ball.x = 0;
+  switch (G.state)
+  {
+    case States.START:
+      {
+        G.p1.score = 0; G.p2.score = 0;
+        if (keys_down.size > 0) G.state = States.PLAYING;
+        break;
+      }
+    case States.PLAYING:
+      {
+        if (keys_down.has('KeyW') && G.p1.z > -5)
+          move(G.p1, {z: -1}, G.p1.speed, delta_ms);
+        if (keys_down.has('KeyS') && G.p1.z < 5)
+          move(G.p1, {z: 1}, G.p1.speed, delta_ms);
+        if (keys_down.has('KeyI') && G.p2.z > -5)
+          move(G.p2, {z: -1}, G.p2.speed, delta_ms);
+        if (keys_down.has('KeyK') && G.p2.z < 5)
+          move(G.p2, {z: 1}, G.p2.speed, delta_ms);
+        if (G.ball.dir.x > 0) collide(G.ball, G.p1);
+        if (G.ball.dir.x < 0) collide(G.ball, G.p2);
+        move(G.ball, G.ball.dir, G.ball.speed, delta_ms);
+        if (Math.abs(G.ball.z) > 6.3) {
+          if (G.ball.dir.z < 0 && G.ball.z < 0) G.ball.dir.z *= -1;
+          if (G.ball.dir.z > 0 && G.ball.z > 0) G.ball.dir.z *= -1;
+        }
+        if (Math.abs(G.ball.x) > G.width / 2) {
+          if (G.ball.x < 0) G.p1.score += 1;
+          if (G.ball.x > 0) G.p2.score += 1;
+          G.ball.z = 0; G.ball.x = 0;
+        }
+        // This logic needs to move to a transition state
+        if (Math.max(G.p1.score, G.p2.score) >= 11) {
+          G.state = States.GAME_OVER;
+          setTimeout(() => G.state = States.START, 3000);
+          xhrPost("https://echo.free.beeceptor.com",
+            {P1: G.p1.score, P2: G.p2.score});
+        }
+        break;
+      }
+    case States.GAME_OVER:
+      {
+        break;
+      }
   }
 }
 
@@ -119,21 +119,32 @@ const container = Object.assign(document.createElement('div'), {
 });
 const overlay = Object.assign(document.createElement('div'), {
   style: 'position:absolute; top:0; left:0; width:100%;'
-       + 'height:100%; pointer-events:none; color:white;'
-       + 'font-family:monospace; font-size:24px; z-index:10'
+  + 'height:100%; pointer-events:none; color:white;'
+  + 'font-family:monospace; font-size:24px; z-index:10'
 });
 const scoreDisplay = Object.assign(document.createElement('h1'), {
   style: 'position:absolute; top:20px; left:50%;'
-       + 'transform:translateX(-50%); text-align:center'
+  + 'transform:translateX(-50%); text-align:center'
 });
 
 canvas.parentNode.insertBefore(container, canvas);
 container.append(canvas, overlay);
 overlay.appendChild(scoreDisplay);
 
+S = createScene(canvas, G);
 function loop(current_time_ms) {
   update(current_time_ms);
   scoreDisplay.textContent = `${G.p1.score} | ${G.p2.score}`;
+  switch (G.state) {
+    case States.GAME_OVER:
+      let winner = G.p1.score > G.p2.score ? G.p1 : G.p2;
+      let loser  = G.p1.score < G.p2.score ? G.p1 : G.p2;
+      scoreDisplay.textContent = `${winner.name} wins! ${winner.score} to ${loser.score}`;
+      break;
+    case States.START:
+      scoreDisplay.textContent = `Controls: WS, IK`
+      break;
+  }
   S.lpaddle.position.set(G.p1.x, 0, G.p1.z);
   S.rpaddle.position.set(G.p2.x, 0, G.p2.z);
   S.sphere.position.set(G.ball.x, 0, G.ball.z);
@@ -143,3 +154,47 @@ function loop(current_time_ms) {
 }
 
 requestAnimationFrame(loop);
+
+function createScene(canvas, G)
+{
+  b = BABYLON; v3 = BABYLON.Vector3;
+
+  Engine = new b.Engine(canvas);
+  S = new b.Scene(Engine);
+  S.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+
+  S.camera = new b.FreeCamera("camera", new v3(0, 15, 8), S);
+  S.ground = b.MeshBuilder.CreateGround("ground",
+    {width: G.width, height: PLAYING_FIELD_HEIGHT}, S);
+  S.sphere = b.MeshBuilder.CreateSphere("sphere", {diameter: G.ball.diameter}, S);
+
+  S.camera.setTarget(v3.Zero());
+  S.ground.position = new v3(0,-1,0);
+
+  S.light3 = new b.PointLight("light3", new v3(0, 2, 0), S);
+  S.light3.intensity = 0.1;
+
+  Object.assign(S, {
+    lpaddle: b.MeshBuilder.CreateCapsule("lpaddle", {
+      height: G.p1.height, radius: G.p1.width,
+      orientation: new v3(0, 0, 1)
+    }, S),
+    rpaddle: b.MeshBuilder.CreateCapsule("rpaddle", {
+      height: G.p2.height, radius: G.p2.width,
+      orientation: new v3(0, 0, 1)
+    }, S)
+  });
+
+  const paddleColor = new b.Color3(1,1,1);
+  [S.lpaddle, S.rpaddle, S.sphere].forEach(mesh => {
+    mesh.material = new b.StandardMaterial(`${mesh.name}_mat`, S);
+    mesh.material.emissiveColor = paddleColor;
+  });
+  return (S);
+}
+
+function xhrPost(url, body) {
+  req = new XMLHttpRequest();
+  req.open("POST", url);
+  req.send(JSON.stringify(body));
+}
